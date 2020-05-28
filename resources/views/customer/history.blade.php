888191710bbac68353ab645630330b0f06b0c5ce
Farhan Karomi <farhan10@mhs.unsyiah.ac.id>
2020-05-28 07:16:33 +0700

    update order view

--- resources/views/customer/history.blade.php
+++ resources/views/customer/history.blade.php
@@ -1,10 +1,11 @@
 @extends ('layouts.app')
-@section('title', 'History')
+@section('title', 'Order')
 
 @section('content')
-Pemesanan berhasil! <br>
+Pemesanan berhasil ! <br>
 {{ $data['bobot'] }} kg ikan {{  $data['namaikan'] }} <br>
- pasar {{ $market->name }} <br>
+Harga:  Rp  <br>
+Pasar {{ $market->name }} <br>
 Alamat: {{ $market->address }} <br>
 penjual: {{ $pedagang->firstname }} hp: {{ $pedagang->phonenumber }} <br>
 order id: {{ $data['code'] }}
